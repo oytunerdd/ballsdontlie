fd9e97008a2880c2c83903e607982d1b278b503b
oytunerdd <oytunerdogan@gmail.com>
2024-04-29 20:37:07 +0300

    Screenshots added for running examples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: 'tr-TR', component: AppComponent },
-    { path: 'en-US', component: AppComponent },
+    { path: ':lang', component: AppComponent },
 
 
 ];
